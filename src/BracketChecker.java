ccee7314de7a21eb0d771af59569c4befa22e4a6
KatherineP3101 <kpanova2k@gmail.com>
2025-05-21 17:25:40 +0200

    Initial commit.

---
+++ src/BracketChecker.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class BracketChecker {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (!isMatchingPair(top, c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private static boolean isMatchingPair(char opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+                (opening == '[' && closing == ']') ||
+                (opening == '{' && closing == '}');
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Exercise 1
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] expectedNums = {0, 1, 2, 3, 4, 0, 0, 0, 0, 0};
+        int[] result = removeDuplicates(nums);
+
+        System.out.println(Arrays.equals(result, expectedNums));
+
+        // Exercise 2
+        System.out.println(BracketChecker.isValid("([)]"));
+        System.out.println(BracketChecker.isValid("(]"));
+        System.out.println(BracketChecker.isValid("()[]{}"));
+        System.out.println(BracketChecker.isValid("()"));
+
+        // Exercise 3
+        String input = "hello world";
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        for (char c : input.toCharArray()) {
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
+            }
+        }
+
+        for (char c : input.toCharArray()) {
+            charCountMap.merge(c, 1, Integer::sum);
+        }
+
+        // Exercise 4
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println("Original list: " + list); // [1, 2, 3, 4, 5, 6]
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            int num = iterator.next();
+            if (num % 2 == 0) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println("List after deleting all the even numbers: " + list);
+
+    }
+
+    private static int[] removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return nums;
+        }
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        for (int k = i + 1; k < nums.length; k++) {
+            nums[k] = 0;
+        }
+
+        return nums;
+    }
+
+
+}
